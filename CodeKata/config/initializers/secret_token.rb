b2bdc82415a917bb216c5ce22e9378f661dd8eac
Jdowns123 <jason.downs@octanner.com>
2013-06-25 12:52:46 -0600

    initial commit

---
+++ CodeKata/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeKata::Application.config.secret_token = '01e26b14bf593813eaa7c470fbbfc7b9e349b7caa6072aedb8d75afa61da3156b66a6daba2747299890f9d058031ae6aa090fbffeb9f5eae10dfc5b193968a1a'
